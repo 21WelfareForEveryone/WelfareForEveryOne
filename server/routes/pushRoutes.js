c3bc9e93817a6d9bc45b66462d0c19460794d00a
KWON TAE HOON <33998183+Oxymoron957@users.noreply.github.com>
2021-12-24 15:16:44 +0900

    Feat: TW-148 Pushararm modify
    푸시알림 기능 변경 전 불필요해진 코드 삭제 & 주석 추가

--- server/routes/pushRoutes.js
+++ server/routes/pushRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pushController = require('../controllers/pushController');
 
-router.post('/push/getinfo', pushController.getInfo);
-router.post('/push/toggle', pushController.pushToggle);
 router.post('/push/getPushAlarm', pushController.getPushAlarm);
 
 module.exports = router;
