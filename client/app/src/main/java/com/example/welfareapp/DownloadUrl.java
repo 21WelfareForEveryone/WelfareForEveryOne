1c7435c60aec5252485701ad563e26c082b57607
KIM JINSU <wlstn5376@gmail.com>
2021-12-28 02:06:31 +0900

    [FE]Feat/TW-138:Map UI 고도화

--- client/app/src/main/java/com/example/welfareapp/DownloadUrl.java
+++ client/app/src/main/java/com/example/welfareapp/DownloadUrl.java
@@ -1,8 +1,17 @@
 package com.example.welfareapp;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,51 +21,39 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import noman.googleplaces.PlaceType;
+
 public class DownloadUrl
 {
-    public String ReadTheURL(String placeURL) throws IOException
-    {
-        String Data = "";
-        InputStream inputStream = null;
-        HttpURLConnection httpURLConnection = null;
-
-        try
-        {
-            URL url = new URL(placeURL);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.connect();
-
-            inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuffer stringBuffer = new StringBuffer();
-
-            String line = "";
-
-            while ( (line = bufferedReader.readLine()) != null )
-            {
-                stringBuffer.append(line);
-            }
-
-            Data = stringBuffer.toString();
-            bufferedReader.close();
+    public void ReadUrl(String url){
+        try{
+            JsonObjectRequest urlData = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try{
+                        Log.v("MapActivity url test ",response.toString());
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                        Log.v("MapActivity url test Exception error", e.getMessage());
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                    Log.v("MapActivity url test Volley Error", error.toString());
+                }
+            });
+            urlData.setShouldCache(false);
+            AppHelper.requestQueue.add(urlData);
         }
-        catch (MalformedURLException e)
-        {
-            e.printStackTrace();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            inputStream.close();
-            httpURLConnection.disconnect();
+        catch(Exception err){
+            err.printStackTrace();
+            Log.e("MapActivity download read the url error occur", err.getMessage());
         }
 
-        return Data;
     }
-
     public String getUrl(String placeType, int searchRadius, String placeAPIKey, LatLng position){
         StringBuilder googleURL = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
         double latitide = position.latitude;
@@ -66,6 +63,7 @@ public class DownloadUrl
         googleURL.append("&type=" + placeType);
         googleURL.append("&sensor=true");
         googleURL.append("&key=" + placeAPIKey);
+        googleURL.append("&rankby=distance");
         Log.d("MapActivity DownloadUrl class", "url = " + googleURL.toString());
         return googleURL.toString();
     }
