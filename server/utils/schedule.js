0aa465174827b7089d96fc0f01519b322e76ccf2
KWON TAE HOON <33998183+Oxymoron957@users.noreply.github.com>
2022-01-08 15:20:24 +0900

    [Feat][TW-148] 푸시알림 추천 알고리즘 코드 적용
    푸시알림으로 매일 아침 8시에 보내는 복지 알림 정보를 사용자가 설정한 관심 복지 카테고리에 속한 복지 정보들 중 찜 순위 상위 10개 중에서 랜덤으로 하나로 보내는 알고리즘을 구현하여 적용하였습니다.

--- server/utils/schedule.js
+++ server/utils/schedule.js
@@ -7,15 +7,10 @@ const schedule = require('node-schedule');
 const admin = require('firebase-admin');
 const { Op } = require("sequelize");
 
-let serviceAccount = require('./config/hazel-cedar-312311-firebase-adminsdk-75xw8-efe9c58e9f.json')
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
 module.exports = {
   check: () => {
     schedule.scheduleJob('0 0 8 * * *', async()=>{
-    // schedule.scheduleJob('5 * * * * *', async()=>{
+    // schedule.scheduleJob('* * * * * *', async()=>{
         
         // 현재 시간
         const curr = new Date();
@@ -23,44 +18,66 @@ module.exports = {
         const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
         const kr_curr = new Date(utc + (KR_TIME_DIFF));
 
-        console.log(kr_curr);
-        
+        // console.log(kr_curr);
         PushAlarm.findAll({raw: true})
-        .then(users => {
+        .then(users=>{
             users.forEach(user => {
-                console.log(user);
-                User.findByPk(user.user_id)
+                User.findOne({where: {user_id:user.user_id}, raw: true})
                 .then(userinfo=>{
                     const fcm_token = userinfo.user_mToken;
-                    Welfare.findAll({order: [['like_count', 'DESC']], raw: true})
-                    .then(welfare_info=>{
-                        console.log(welfare_info[0].title);
-                        let message = 
-                        {
-                            notification : {
-                                title: '모두를 위한 복지 알림입니다.',
-                                body: welfare_info[0].title,
-                            },
-                            token: fcm_token
-                        }
-
-                        admin
-                        .messaging()
-                        .send(message)
-                        .then(function(response){
-                            console.log('Successfully sent message:', response)
-                            // return res.status(200).json({success: true})
-                        })
-                        .catch(function(err) {
-                            console.log('Error Sending message!!! : ', err)
-                            // return res.status(400).json({success: false})
+                    // console.log(userinfo);
+                    User_interest.findAll({where: {user_id:userinfo.user_id}, raw: true})
+                    .then(interests=>{
+                        let category_list = []
+                        interests.forEach(category => {
+                            category_list.push(category.category_id)
                         });
+                        return category_list
+                    })
+                    .then(category_list=>{
+                        Welfare_category.findAll({where: {category_id: category_list}, raw: true})
+                        .then(welfare_category=>{
+                            let welfare_list = [];
+                            // console.log(welfare_category);
+                            welfare_category.forEach(welfare => {
+                                if(!welfare_list.includes(welfare.welfare_id)){
+                                    welfare_list.push(welfare.welfare_id)
+                                } 
+                            });
+                            
+                            return welfare_list;
+                        })
+                        .then(welfare_list=>{
+                            Welfare.findAll({where: {welfare_id:welfare_list}, order: [['like_count', 'DESC']], raw:true, limit :10})
+                            .then(result=>{
+                                let rand_0_9 = Math.floor(Math.random() * 10);
+                                // console.log(fcm_token, result[rand_0_9].title);
+                                let message = 
+                                {
+                                    notification : {
+                                        title: '모두를 위한 복지 알림입니다.',
+                                        body: result[rand_0_9].title,
+                                    },
+                                    token: fcm_token
+                                }
+                                admin
+                                .messaging()
+                                .send(message)
+                                .then(function(response){
+                                    console.log('Successfully sent message:', response)
+                                    // return res.status(200).json({success: true})
+                                })
+                                .catch(function(err) {
+                                    console.log('Error Sending message!!! : ', err)
+                                    // return res.status(400).json({success: false})
+                                });
+                            })
+                        })
                     })
                 })
-                });
 
+            })
         })
-
     })
 
     }
